2c538139147aadb8e6afbb142772e4ec026b90cb
JP do Ó <166522962+JPdoO@users.noreply.github.com>
2024-04-25 09:01:00 -0300

    Update areaperimetroretangulo.py

--- areaperimetroretangulo.py
+++ areaperimetroretangulo.py
@@ -1,5 +1,7 @@
 altura = float(input("Digite a altura do retângulo: "))
 largura = float(input("Digite a largura do retângulo: "))
+
 perimetro = (altura * 2) + (largura * 2)
 area = largura * altura
+
 print("O perímetro do retângulo é de %f, e sua área é de %f" %(perimetro, area))
