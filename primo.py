4bc49aea6771ac2849e7b69b2a41f22d20261300
JP do Ó <166522962+JPdoO@users.noreply.github.com>
2024-04-25 09:16:12 -0300

    Add files via upload

---
+++ primo.py
@@ -0,0 +1,19 @@
+num = int(input("Digite o número aqui:"))
+divisores = 0
+
+for divisor in range(1, num):   
+    if num % divisor == 0:  
+        divisores = divisores + 1
+        if divisores > 1:   
+            break
+
+if divisores > 1:   
+    print("Este número não é primo.")
+elif num == 1:  
+    print("Caso especial. Você digitou o número um.")
+elif num == 0:  
+    print("Caso especial. Você digitou o número zero.")
+elif num < 0:   
+    print("Caso especial. Você digitou um número negativo.")
+else:   
+    print("Este número é primo.")
