0aacffc4eb967fb97b814d6feec7abd4fa94845b
JP do Ó <166522962+JPdoO@users.noreply.github.com>
2024-04-25 09:02:58 -0300

    Update jurosimples.py

--- jurosimples.py
+++ jurosimples.py
@@ -1,5 +1,7 @@
 investimento = float(input("Digite o valor do investimento inicial: "))
 tempo = int(input("Digite o tempo em anos: "))
 taxa = int(input("Digite a porcentegem da taxa de juros: "))
+
 jurosimples = (investimento * tempo * taxa) / 100
+
 print("O juro simples é de ", jurosimples)
