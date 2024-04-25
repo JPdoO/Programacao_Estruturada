bac31a83eebfab26401bc5dfda1cd8d9f823dd25
JP do Ó <166522962+JPdoO@users.noreply.github.com>
2024-04-25 09:04:56 -0300

    Update triangulinho.py

--- triangulinho.py
+++ triangulinho.py
@@ -1,6 +1,7 @@
 lado1 = float(input("Digite o primeiro lado do triângulo: "))
 lado2 = float(input("Digite o segundo lado do triângulo: "))
 lado3 = float(input("Digite o terceiro lado do triângulo: "))
+
 if lado1 + lado2 < lado3 or lado1 + lado3 < lado2 or lado2 + lado3 < lado1: 
     print("Isto não é um triângulo.")
 elif lado1 == lado2 and lado1 == lado3: 
