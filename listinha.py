21eaa872831b4a51527ba350cc5f7e26db5a5bbc
JP do Ó <166522962+JPdoO@users.noreply.github.com>
2024-06-11 10:33:20 -0300

    Update listinha.py

--- listinha.py
+++ listinha.py
@@ -1,7 +1,9 @@
 lista = []
 
-for presença in range (1, 11):  
-    nome = str(input("Digite o nome do aluno:"))
+for presença in range (1, 15):  
+    nome = str(input("Digite o nome do aluno (digite a palavra fim para quando não houver mais alunos presentes):"))
     lista.append (nome)
+    if nome == "fim":    
+        break
 
 print ("Os alunos presentes são: ", lista)
