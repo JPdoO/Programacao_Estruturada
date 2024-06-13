ac47e29f38292f532ad23d0ca8aafdf0666f11b2
JP do Ó <166522962+JPdoO@users.noreply.github.com>
2024-06-13 10:17:08 -0300

    Add files via upload

---
+++ listinha2_eletric bogaloo.py
@@ -0,0 +1,25 @@
+comidas = ["leite", "couve", "tomate", "garfo", "faca", "refrigerante"]
+bebidas = ["uva", "colher", "vinho", "cerveja", "banana"]
+talheres = ["arroz", "concha", "whisky", "vodka", "feijão", "colher de chá"]
+
+comidas.pop (0)
+comidas.pop (2)
+comidas.pop (2)
+comidas.pop (2)
+
+bebidas.pop (0)
+bebidas.pop (0)
+bebidas.pop (2)
+
+talheres.pop (0)
+talheres.pop (1)
+talheres.pop (1)
+talheres.pop (1)
+
+comidas.extend (["uva", "banana", "arroz", "feijão"])
+bebidas.extend (["leite", "refrigerante", "whisky", "vodka"])
+talheres.extend (["garfo", "faca", "colher"])
+
+print ("Comidas: ", comidas)
+print ("Bebidas: ", bebidas)
+print ("Talheres: ", talheres)
