ae335c66683af01302ddeff07dbfa321b72ea9dc
JP do Ó <166522962+JPdoO@users.noreply.github.com>
2024-06-18 15:03:35 -0300

    Update listinha3_avingançadalistinha.py

--- listinha3_avingançadalistinha.py
+++ listinha3_avingançadalistinha.py
@@ -1,7 +1,13 @@
 lista1 = [2, 3, 5, 7]
 lista2 = [1, 3, 5, 7]
-lista3 = []
 
-lista3.extend (lista1 + lista2)
+listacombine = []
 
-print (lista3)
+listacombine.extend (lista1 + lista2)
+print (listacombine)
+
+listacombine.pop (5)
+listacombine.pop (5)
+listacombine.pop (5) 
+
+print (listacombine)
