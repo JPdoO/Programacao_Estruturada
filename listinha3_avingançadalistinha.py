f127e6de664eb22fc19eec0a043ee36b1c4f4033
JP do Ó <166522962+JPdoO@users.noreply.github.com>
2024-06-20 08:53:31 -0300

    Update listinha3_avingançadalistinha.py

--- listinha3_avingançadalistinha.py
+++ listinha3_avingançadalistinha.py
@@ -1,13 +1,28 @@
-lista1 = [2, 3, 5, 7]
-lista2 = [1, 3, 5, 7]
+lista1 = []
+contador1 = 0
+lista2 = []
+contador2 = 0
 
 listacombine = []
 
+while contador1 >= 0:   
+    num1 = int(input("Digite um número para incerir na primeira lista aqui (Digite o número 0 para encerrar o processo):"))
+    if num1 == 0:   
+        break
+    lista1.append (num1)
+    contador1 = contador1 + 1
+
+while contador2 >= 0:   
+    num2 = int(input("Digite um número para incerir na segunda lista aqui (Digite o número 0 para encerrar o processo):"))
+    if num2 == 0:   
+        break
+    lista2.append (num2)
+    contador2 = contador2 + 1
+
 listacombine.extend (lista1 + lista2)
-print (listacombine)
+print ("Os números da primeira lista são:", lista1)
+print ("Os números da segunda lista são:", lista2)
+print ("Os números das duas listas combinadas são:", listacombine)
 
-listacombine.pop (5)
-listacombine.pop (5)
-listacombine.pop (5) 
-
-print (listacombine)
+listacombine = set (listacombine)
+print ("Os números das duas listas depois de remover duplicatas são:", listacombine)
